6be846cbd41b8ec813317043c2f049f1359f5547
Filip911 <filipworkrate@gmail.com>
2019-12-03 21:34:54 +0100

    for today enough

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
 </head>
 <body>
     
 <header>
-    <nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a href="#">
         <img src="img/maestro.jpg" alt="lgo">
         </a>
@@ -21,7 +22,13 @@
         </ul>
         <div>
             <form action="includes/login.inc.php" method="POST">
-                <input type="text">
+                <input type="text" name="id" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">SignUp</a>
+            <form action="includes/logout.inc.php" method="POST">
+                <button type="submit" name="logout-submit">Logout</button>
             </form>
         </div>
     </nav>

9ff33f8ec5893545521cbe66450bca6354e45242
Filip911 <filipworkrate@gmail.com>
2019-12-04 11:15:46 +0100

    started

--- header.php
+++ header.php
@@ -4,32 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
+    <title>ProjectX</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://bootswatch.com/3/spacelab/bootstrap.min.css">
 </head>
 <body>
     
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a href="#">
-        <img src="img/maestro.jpg" alt="lgo">
-        </a>
-        <ul>
+    <nav class="navbar navbar-default">
+    <a href="" class="navbar-brand">?LOGO?</a>
+        <ul class="nav navbar-nav">
             <li><a href="#">Home</a></li>  
             <li><a href="#">Portfolio</a></li> 
             <li><a href="#">About Me</a></li> 
-            <li><a href="#">Contact</a></li>       
+            <li><a href="#">Contact</a></li> 
         </ul>
-        <div>
-            <form action="includes/login.inc.php" method="POST">
-                <input type="text" name="id" placeholder="Username">
-                <input type="password" name="pwd" placeholder="Password">
-                <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">SignUp</a>
-            <form action="includes/logout.inc.php" method="POST">
-                <button type="submit" name="logout-submit">Logout</button>
-            </form>
-        </div>
+       
     </nav>
 </header>
--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 ?>
 
     <main>
-       <p>You are logged out!</p>
-       <p>You are logged in!</p>
+
     </main>
 
  <?php 

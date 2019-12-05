3597601d1c5a8f077c1d31d1cc8d7cc2eb95f27c
Filip911 <filipworkrate@gmail.com>
2019-12-05 11:00:21 +0100

    making progress

--- header.php
+++ header.php
@@ -11,20 +11,43 @@
 <body>
     
 <header>
-    <nav class="navbar navbar-default">
-    <a href="" class="navbar-brand">?LOGO?</a>
-        <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>  
-            <li><a href="#">Portfolio</a></li> 
-            <li><a href="#">About Me</a></li> 
-            <li><a href="#">Contact</a></li> 
-        </ul>   
-        <div class="header-login ">
-            <form class="" action="includes/login.inc.php" method="POST">
-            <input type="text" name="id" placeholder="Username">
-            <input type="password" name="pwd" placeholder="Password">
-        </div>     
-    </nav>
-        
-</header>
+<div class="sticky-header-navbar-container">
+        <div class="well">
+            <div class="navbar navbar-default navbar-fixed-top" id="navbar">
+                <a class="navbar-brand" href=""><img src="img/gear.png"></a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    
+                </ul>
+                <ul class="nav navbar-nav navbar-right line-inline">
+                    
+                    <li>
 
+                        <!-- Changes I made start here -->
+
+                        <form class="form-signin navbar-form">
+                            
+                            <label for="inputEmail" class="sr-only">Email address</label>
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Email Address" required autofocus>
+                            <label for="inputPassword" class="sr-only">Password</label>
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                                <span><button type="submit" name="login-submit">Login</button></span>
+                             <span><a href="signup.php" class="">SignUp</a><span>
+                            
+                          <form action="includes/logout.inc.php" method="POST" class="">
+                                <button type="submit" class="btn btn-default" name="logout-submit">Logout</button>
+                          </form> 
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
